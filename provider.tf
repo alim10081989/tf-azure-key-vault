fa24c2b62dc4e9260dcea012c31e2dc1da67b915
alim10081989 <alim.azad@gmail.com>
2023-04-07 10:35:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,19 @@
 terraform {
+  required_version = ">=1.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "3.47.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
